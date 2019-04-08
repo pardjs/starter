8485976c2203358d482ead52b4c788e8201cc688
Bai Guodong <bgd_sh@hotmail.com>
2019-04-08 17:58:45 +0800

    feat(doc): docs for api and project

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 config();
 
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { PORT } from './constants';
 import {
@@ -10,13 +12,25 @@ import {
   ValidationPipe,
   HttpExceptionFilter,
 } from '@pardjs/common';
+// tslint:disable-next-line:no-var-requires
+const { version } = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  const options = new DocumentBuilder()
+    .setTitle('Pardjs starter')
+    .setDescription('The Pardjs starter API description')
+    .setVersion(version)
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  const apiDocPath = 'api-doc';
+  SwaggerModule.setup(apiDocPath, app, document);
   await app.listen(PORT);
   logger.info(`service started at ${PORT}`);
+  logger.info(`find api doc at http://0.0.0.0:${PORT}/${apiDocPath}`);
 }
 bootstrap();
